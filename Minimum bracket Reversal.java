ffab9713ea00c7c54e9fa81e4ee45e15ea551899
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-04 20:39:09 +0530

    Create Minimum bracket Reversal.java

---
+++ Minimum bracket Reversal.java
@@ -0,0 +1,10 @@
+/*  
+
+Problem :
+
+Given a string expression which consists only ‘}’ and ‘{‘. The expression may not be balanced. You need to find the minimum number of bracket reversals which are required to make the expression balanced.
+Return -1 if the given expression can't be balanced.
+
+*/
+
+
