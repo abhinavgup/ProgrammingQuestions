f7e33c89898daf333e1b23aa44139814dcf3903c
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-07 23:06:31 +0530

    Update Print Subsets of Array.java

--- Print Subsets of Array.java
+++ Print Subsets of Array.java
@@ -5,3 +5,13 @@ Given an integer array (of length n), find and print all the subsets of input ar
 
 //
 
+public class solution {
+	public static void printSubsets(int input[],int si,int outputso[]) {
+		
+      
+      if (input.length==si){
+        
+       
+        for(int i=0;i<outputso.length;i++){
+          System.out.print(outputso[i]+" ");
+        }
