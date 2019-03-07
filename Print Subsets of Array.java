65b2382c6722c68ed34730098fe862711742cd90
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-07 23:11:51 +0530

    Update Print Subsets of Array.java

--- Print Subsets of Array.java
+++ Print Subsets of Array.java
@@ -10,19 +10,24 @@ public class solution {
 		
       
       if (input.length==si){
-        
-       
+               
         for(int i=0;i<outputso.length;i++){
           System.out.print(outputso[i]+" ");
         }
 	   
 	System.out.println();
         return;
-      
-      }
+            }
 	
        int[]  newoutputso=new int[outputso.length+1];
             
       	  printSubsets(input,si+1,outputso);
+	newoutputso[outputso.length]=input[si];
+     for(int i=0;i<outputso.length;i++){
+                                           
+      newoutputso[i]=outputso[i];
+     }
+      printSubsets(input,si+1,newoutputso);
+    }
       
       		
