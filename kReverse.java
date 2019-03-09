91bcaf7ed2e6443e3948cd6438a7339ab4b36043
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-09 23:44:19 +0530

    Create kReverse.java

---
+++ kReverse.java
@@ -0,0 +1,11 @@
+/*
+Problem:
+
+Implement kReverse( int k ) in a linked list i.e. you need to reverse first K elements then reverse next k elements 
+and join the linked list and so on.
+Indexing starts from 0. If less than k elements left in the last, you need to reverse them as well. If k is greater 
+than length of LL, reverse the complete LL.
+You don't need to print the elements, just return the head of updated LL.
+
+*/
+
