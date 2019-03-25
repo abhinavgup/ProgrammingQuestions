2c388eba38f95894f656ea4a0fee1f21b485affe
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-25 23:57:21 +0530

    Create CoinTower.java

---
+++ CoinTower.java
@@ -0,0 +1,8 @@
+ /*
+Question-
+
+Whis and Beerus are playing a new game today . They form a tower of N coins and make a move in alternate turns . 
+Beerus being the God plays first . In one move player can remove 1 or X or Y coins from the tower .
+The person to make the last move wins the Game . Can you find out who wins the game ?
+
+*/
